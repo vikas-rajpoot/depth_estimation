7c5c4bfb82aa1e56a07a0cf373bc4e4178c02f9f
Vikas Rajpoot <vikasrajpoot0100@gmail.com>
2023-07-21 21:42:32 +0530

    adding the ssim index evaluation code.

--- test.py
+++ test.py
@@ -5,6 +5,12 @@ import cv2
 from utils_test import DepthDataset
 from utils_test import ToTensor
 from torchvision import transforms
+import numpy as np
+from skimage.metrics import mean_squared_error, structural_similarity
+
+from PIL import Image
+import numpy as np
+
 
 
 loc_img="./test_img_1" 
@@ -96,14 +102,77 @@ def test_model(file):
         plt.imsave('./results/%d_image.jpg' %i, s_img)   
 
 
+def mse_and_ssim(ground_truth, predicted):
+    """calculate the Mean squred error and the ssim index. 
+
+    Args:
+        ground_truth (np.ndarray): _description_
+        predicted (np.ndarray): _description_
+
+    Raises:
+        ValueError: if image size is not match. 
+
+    Returns:
+        int: returns the mes and ssim. 
+    """
+    # Ensure both maps have the same shape
+    if ground_truth.shape != predicted.shape:
+        raise ValueError("Ground truth and predicted depth maps must have the same shape.")
+
+    # Convert the depth maps to float32 if necessary
+    ground_truth = ground_truth.astype(np.float32)
+    predicted = predicted.astype(np.float32)
+
+    # Calculate Mean Squared Error (MSE)
+    mse = mean_squared_error(ground_truth, predicted)
+
+    # Calculate Structural Similarity Index (SSIM)
+    ssim = structural_similarity(ground_truth, predicted, data_range=predicted.max() - predicted.min())
+
+    return mse, ssim
+
+def evaluation():
+    """
+        This function is used to compare the SSIM and MSE. 
+    """
+    # Open the image using Pillow (PIL)
+    image_path = './results/1_depth.jpg'
+    image = Image.open(image_path)
+
+    # Convert the image to grayscale
+    gray_image = image.convert("L")
+
+    # Convert the grayscale image to a 2D NumPy array
+    predicted_depth_map = np.array(gray_image)
+        
+        
+    # Open the image using Pillow (PIL)
+    image_path = './original_depth/depth1.png'
+    image = Image.open(image_path)
+
+    # Convert the image to grayscale
+    gray_image = image.convert("L")
+
+    # Convert the grayscale image to a 2D NumPy array
+    ground_truth_depth_map = np.array(gray_image)
+    
+    mse, ssim = mse_and_ssim(ground_truth_depth_map, predicted_depth_map)
+
+    print("Mean Squared Error (MSE):", mse)
+    print("Structural Similarity Index (SSIM):", ssim)    
+    
+
+
+
 if __name__=='__main__':
     # un-comment the felloing line to visualize the dataset. 
     # visualze_test_dataset() 
     
     # un-comment the to test the trained model.
-    file = './models/19.pth'
-    test_model(file)  
+    # file = './models/19.pth'
+    # test_model(file)  
     
-    
+    # Un-comment the fellowing line to evaluate the model. 
+    evaluation()  
     
 
